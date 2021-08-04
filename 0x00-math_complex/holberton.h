8d1d417ea95b8a1063cc64eef75af9681afa6164
nitaly31 <geraldine.nitaly@gmail.com>
2021-08-03 18:58:14 -0700

    adding structure for complex math projects in library

--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -3,6 +3,21 @@
 
 #include <stdio.h>
 
+/**
+ * struct complex - math complex
+ * @re: real number
+ * @im: imaginary number
+ *
+ * Description: number structure for complex mathematical operations
+ */
+
+typedef struct complex
+{
+	double re;
+	double im;
+} complex;
+
+
 void display_complex_number(complex c);
 complex conjugate(complex c);
 double modulus(complex c);
@@ -11,6 +26,6 @@ void addition(complex c1, complex c2, complex *c3);
 void substraction(complex c1, complex c2, complex *c3);
 void multiplication(complex c1, complex c2, complex *c3);
 void division(complex c1, complex c2, complex *c3);
-void complex_from_mod_arg (double m, double arg, complex *c3);
+void complex_from_mod_arg(double m, double arg, complex *c3);
 
 #endif
